059903ac4a92b65d7f404d38391b9d20c3962945
Dan Herman <dherman@intratechs.com>
2015-01-30 11:02:00 -0500

    cleaned up code a bit

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,63 +1,68 @@
 
 # Install awslogs on server
-	if platform_family?('rhel')    
-		yum_package "awslogs" do
-		  action :install
-		end
-	end
-
-  template "/tmp/awslogs.conf" do
-    source "awslogs.conf.erb"
-    owner "root"
-    group "root"
-    mode 0755
-		variables(:awslogs => node[:awslogs])
-    action :create
+if platform_family?('rhel')
+  yum_package 'awslogs' do
+    action :install
   end
+end
 
-  execute "download awslogs setup script" do
-    if platform_family?('debian')
-			command "wget https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py"
-			action :run
-    end
-	end
-	execute "run awslogs setup script" do
-    if platform_family?('debian')
-			command "python ./awslogs-agent-setup.py -n -r us-east-1 -c /tmp/awslogs.conf"
-      action :run
-    end
-	end	
+etc_folder = '/etc/awslogs'
+state_path = '/var/lib/awslogs'
+if platform_family?('debian')
+  etc_folder = '/var/awslogs/etc'
+  state_path = '/var/awslogs/state'
+end
 
+template '/tmp/awslogs.conf' do
+  source 'awslogs.conf.erb'
+  owner 'root'
+  group 'root'
+  mode 0755
+  variables(state_path: state_path)
+  action :create
+end
+
+execute 'download awslogs setup script' do
+  if platform_family?('debian')
+    command 'wget https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py'
+    action :run
+  end
+end
+
+execute 'run awslogs setup script' do
+  if platform_family?('debian')
+    command 'python ./awslogs-agent-setup.py -n -r us-east-1 -c /tmp/awslogs.conf'
+    action :run
+  end
+end
 
 # Template for /etc/awslogs/awscli.conf
- etc_folder = '/etc/awslogs'
- state_path = '/var/lib/awslogs/agent-state'
- if platform_family?('debian')
-   etc_folder = '/var/awslogs/etc'
-   state_path = '/var/awslogs/state'
- end
-
- template "#{etc_folder}/awscli.conf" do
-    source "awscli.conf.erb"
-    owner "root"
-    group "root"
-    mode 0755
-		variables(:awslogs => node[:awslogs], :state_path => state_path)
-    action :create
-  end
+template "#{etc_folder}/awscli.conf" do
+  source 'awscli.conf.erb'
+  owner 'root'
+  group 'root'
+  mode 0755
+  action :create
+end
 
 # Template for /etc/awslogs/awslogs.conf
- template "#{etc_folder}/awslogs.conf" do
-    source "awslogs.conf.erb"
-    owner "root"
-    group "root"
-    mode 0755
-		variables(:awslogs => node[:awslogs])
-    action :create
+template "#{etc_folder}/awslogs.conf" do
+  source 'awslogs.conf.erb'
+  owner 'root'
+  group 'root'
+  mode 0755
+  variables(state_path: state_path)
+  action :create
+end
+
+execute 'add agent-state file' do
+  if platform_family?('debian')
+    command "touch #{state_path}/agent-state"
+    action :run
   end
+end
 
 # Start the awslogs service and make sure it starts at boot
-  service "awslogs" do
-    action [:enable, :start]
-  end
-
+service 'awslogs' do
+  action [:enable, :start]
+end
