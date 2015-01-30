046a988dc1f670e65b0a120e08293347d9ca766d
Dan Herman <dherman@intratechs.com>
2015-01-30 11:39:07 -0500

    Added cleanup for tmp files

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,24 +13,26 @@ if platform_family?('debian')
   state_path = '/var/awslogs/state'
 end
 
-template '/tmp/awslogs.conf' do
-  source 'awslogs.conf.erb'
-  owner 'root'
-  group 'root'
-  mode 0755
-  variables(state_path: state_path)
-  action :create
+if platform_family?('debian')
+  template '/tmp/awslogs.conf' do
+    source 'awslogs.conf.erb'
+    owner 'root'
+    group 'root'
+    mode 0755
+    variables(state_path: state_path)
+    action :create
+  end
 end
 
-execute 'download awslogs setup script' do
-  if platform_family?('debian')
+if platform_family?('debian')
+  execute 'download awslogs setup script' do
     command 'wget https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py'
     action :run
   end
 end
 
-execute 'run awslogs setup script' do
-  if platform_family?('debian')
+if platform_family?('debian')
+  execute 'run awslogs setup script' do
     command 'python ./awslogs-agent-setup.py -n -r us-east-1 -c /tmp/awslogs.conf'
     action :run
   end
@@ -55,8 +57,8 @@ template "#{etc_folder}/awslogs.conf" do
   action :create
 end
 
-execute 'add agent-state file' do
-  if platform_family?('debian')
+if platform_family?('debian')
+  execute 'add agent-state file' do
     command "touch #{state_path}/agent-state"
     action :run
   end
@@ -64,5 +66,15 @@ end
 
 # Start the awslogs service and make sure it starts at boot
 service 'awslogs' do
-  action [:enable, :start]
+  action [:enable, :restart]
+end
+
+# Cleanup tmp files
+if platform_family?('debian')
+  file '/tmp/awslogs.conf' do
+    action :delete
+  end
+  file 'awslogs-agent-setup.py' do
+    action :delete
+  end
 end
